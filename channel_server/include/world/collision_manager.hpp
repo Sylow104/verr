790272cc6f694ca1c740bda2291145f079bbb80e
Patrick Ching <burning.codex@gmail.com>
2023-06-08 12:15:53 -0600

    need to convert to map soon

--- channel_server/include/world/collision_manager.hpp
+++ channel_server/include/world/collision_manager.hpp
@@ -1,4 +1,23 @@
 #pragma once
+#include <vector>
+#include <map>
+
+class shaped_entity_i;
+class position_u;
+template<typename T> class quadtree_u
+{
+	public:
+	quadtree_u()
+	{
+		;
+	};
+	~quadtree_u()
+	{
+		;
+	};
+};
+
+class linepath_u;
 
 class collision_m
 {
@@ -6,8 +25,17 @@ class collision_m
 	collision_m();
 	~collision_m();
 
+	void add(shaped_entity_i *shaped_entity);
+	void remove(long entity_id);
+	bool any(position_u *from, position_u *to);
+	bool find(position_u *from, position_u *to, position_u *intersection_out);
+
 	protected:
 
 	private:
+	bool find_intersection(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4, position_u *intersection_out);
+
+	quadtree_u<linepath_u *> *_tree;
+	std::map<long, linepath_u *> _reference;
 	
 };
